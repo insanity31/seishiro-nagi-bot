e7f612e3c2cab01a352f5c9804d3a51d7770bf56
insanity31 <insnitybanez@gmail.com>
2026-01-07 05:35:49 -0400

    Change reaction emoji from 🌸 to 👻

--- plugins/Fun-memes.js
+++ plugins/Fun-memes.js
@@ -2,7 +2,7 @@ import hispamemes from 'hispamemes'
 let handler = async (m, { conn, usedPrefix, command }) => {
 const meme = hispamemes.meme()
 conn.sendFile(m.chat, meme, '', '', m)
-m.react('🌸')
+m.react('👻')
 }
 handler.help = ['meme']
 handler.tags = ['fun']
